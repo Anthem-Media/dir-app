55a7ba5e3f4e430529dfbd56b5f790f098367477
anthem-media <azimuthbeacon@gmail.com>
2026-06-06 13:19:27 -0500

    fix badge display and remove decimals from price in grails tab

--- src/utils/formatters.js
+++ src/utils/formatters.js
@@ -41,6 +41,7 @@ export function formatCurrency(value) {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
+    maximumFractionDigits: 0,
   }).format(value);
 }
 
